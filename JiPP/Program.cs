323f3ec3134b6bcbf3da241673eebecf49d9d01b
Maciek10426 <makru@onet.pl>
2025-03-18 23:56:59 +0100

    init

--- JiPP/Program.cs
+++ JiPP/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj swoje imię:");
+        var name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+
+        int result = 5 + 9;
+        Console.WriteLine("Wynik dodawania: " + result);
+
+        int number = 37;
+        float money = 99.99f;
+        string text = "Przykładowy tekst pisany";
+        bool isLogged = true;
+        char myChar = 'A';
+        decimal price = 199.99m;
+
+        Console.WriteLine("Liczba całkowita: " + number);
+        Console.WriteLine("Liczba zmiennoprzecinkowa (float): " + money);
+        Console.WriteLine("Tekst: " + text);
+        Console.WriteLine("Wartość logiczna: " + isLogged);
+        Console.WriteLine("Pojedynczy znak: " + myChar);
+        Console.WriteLine("Liczba zmiennoprzecinkowa o największej precyzji (decimal): " + price);
+    }
+}
